65b09ff04e23215ff8655f1ced234e7cba069666
FabricioVeloso <fabraccho@gmail.com>
2026-03-02 15:39:24 -0300

    Feat: Keymaps - Set options to avoid weird conflicts.

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -3,3 +3,7 @@
 --  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
+
+-- For conciseness
+local opts = { noremap = true, silent = true }
+
