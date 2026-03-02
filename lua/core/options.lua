ac1f3c7fbb2177510c2324246facefc3877c363f
FabricioVeloso <fabraccho@gmail.com>
2026-03-02 15:27:11 -0300

    Feat: Main Options - Configure undo history (to true).

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -1,3 +1,6 @@
+-- Save undo history
+vim.opt.undofile = true
+
 -- Case-insensitive searching UNLESS \C or one or more capital letters in the search term
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
