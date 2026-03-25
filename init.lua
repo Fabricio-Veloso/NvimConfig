80f8badcfe0d2b25d27ef7e337ec5788d9349313
FabricioVeloso <fabraccho@gmail.com>
2026-03-25 11:25:21 -0300

    Feat: options - Add pt-br to dictionary.

--- init.lua
+++ init.lua
@@ -2,7 +2,7 @@ require("core.options")
 require("core.keymaps")
 
 vim.opt.spell = true
-vim.opt.spelllang = { "en_us" }
+vim.opt.spelllang = { "en_us", "pt_br" }
 
 local uname = vim.loop.os_uname().sysname
 
